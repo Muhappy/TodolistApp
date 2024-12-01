f3530add0ba3487ecf6cf49e73b3ddc0c908aa21
Muhappy <abdillahputra02@gmail.com>
2024-12-01 19:01:34 +0700

    Day1

---
+++ tempData.js
@@ -0,0 +1,80 @@
+export default tempData =[
+    {
+        name: "plan a trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "purple",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Birthday Party",
+        color: "#595bd9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    }
+]
